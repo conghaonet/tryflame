fa12a3d18e69822f91b111c8dc89c77d9fb295e2
conghaonet <hao.cong@app2m.com>
2022-08-04 23:15:29 +0800

    EntityComponent with Tappable

--- lib/mine/try_constant_volume_joint.dart
+++ lib/mine/try_constant_volume_joint.dart
@@ -12,7 +12,7 @@ void main() {
   runApp(GameWidget(game: TryConstantVolumeJoint()));
 }
 
-class TryConstantVolumeJoint extends Forge2DGame with TapDetector {
+class TryConstantVolumeJoint extends Forge2DGame with HasTappables {
   final blobRadius = 6.0;
   final _fpsComponent = FpsTextComponent();
   final List<Wall> walls = [];
@@ -40,12 +40,85 @@ class TryConstantVolumeJoint extends Forge2DGame with TapDetector {
   }
 
   @override
-  void onTapDown(TapDownInfo info) {
-    final velocity = (Vector2.random() - Vector2.random()) * 20;
-    Future(() async {
-      final centerComponent = EntityComponent(info.eventPosition.game, linearVelocity: velocity);
-      await add(centerComponent);
-    });
+  void onTapDown(int pointerId, TapDownInfo info) {
+    super.onTapDown(pointerId, info);
+    if(!info.handled) {
+      final velocity = (Vector2.random() - Vector2.random()) * 20;
+      Future(() async {
+        final centerComponent = EntityComponent(info.eventPosition.game, linearVelocity: velocity);
+        await add(centerComponent);
+      });
+    }
+  }
+}
+
+class EntityComponent extends BodyComponent with Tappable {
+  static const jointCount = 24;
+  final Vector2 entityPosition;
+  final double circleShapeRadius = 5.8;
+  final entityRadius = 6.0;
+  final Color? circleShapeColor;
+  final Vector2? linearVelocity;
+  final List<BlobComponent> blobs = [];
+  final constantVolumeJointDef = ConstantVolumeJointDef()
+    ..frequencyHz = 0.0
+    ..dampingRatio = 0.0;
+
+  EntityComponent(this.entityPosition, {this.circleShapeColor, this.linearVelocity}) {
+    if (circleShapeColor == null) {
+      paint = PaintExtension.random(withAlpha: 0.9, base: 20);
+    } else {
+      paint = Paint()..color = circleShapeColor!;
+    }
+  }
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    blobs.addAll([
+      for (var i = 0; i < jointCount; i++) BlobComponent(i, constantVolumeJointDef, entityRadius, this, jointCount,)
+    ]);
+
+    await gameRef.addAll(blobs);
+    if(constantVolumeJointDef.bodies.length >= 2) {
+      world.createJoint(ConstantVolumeJoint(world, constantVolumeJointDef));
+    }
+  }
+
+  @override
+  Body createBody() {
+    final shape = CircleShape()..radius = circleShapeRadius;
+    final fixtureDef = FixtureDef(
+      shape,
+      restitution: 1,
+      density: 20.0,
+      friction: 0.0,
+    );
+    final bodyDef = BodyDef(
+      userData: this,
+      angularDamping: 0.0,
+      position: entityPosition,
+      type: BodyType.dynamic,
+      linearVelocity: linearVelocity,
+    );
+    final body = world.createBody(bodyDef);
+    body.createFixture(fixtureDef);
+    return body;
+  }
+
+  @override
+  void renderCircle(Canvas canvas, Offset center, double radius) {
+    super.renderCircle(canvas, center, radius);
+    final lineRotation = Offset(0, radius);
+    canvas.drawLine(center, center + lineRotation, Paint()..color = Colors.white);
+  }
+
+  @override
+  bool onTapDown(TapDownInfo info) {
+    removeFromParent();
+    gameRef.removeAll(blobs);
+    info.handled = true;
+    return true;
   }
 }
 
@@ -94,62 +167,5 @@ class BlobComponent extends BodyComponent {
     distanceJointDef.initialize(centerComponent.body, body, centerComponent.body.position, body.position);
     world.createJoint(DistanceJoint(distanceJointDef));
     return body;
-  }
-}
-
-class EntityComponent extends BodyComponent {
-  static const jointCount = 24;
-  final Vector2 entityPosition;
-  final double circleShapeRadius = 5.8;
-  final entityRadius = 6.0;
-  final Color? circleShapeColor;
-  final Vector2? linearVelocity;
-  final constantVolumeJointDef = ConstantVolumeJointDef()
-    ..frequencyHz = 0.0
-    ..dampingRatio = 0.0;
-
-  EntityComponent(this.entityPosition, {this.circleShapeColor, this.linearVelocity}) {
-    if (circleShapeColor == null) {
-      paint = PaintExtension.random(withAlpha: 0.9, base: 20);
-    } else {
-      paint = Paint()..color = circleShapeColor!;
-    }
-  }
-
-  @override
-  Future<void> onLoad() async {
-    super.onLoad();
-    await gameRef.addAll([
-      for (var i = 0; i < jointCount; i++) BlobComponent(i, constantVolumeJointDef, entityRadius, this, jointCount,)
-    ]);
-    if(constantVolumeJointDef.bodies.length >= 2) {
-      world.createJoint(ConstantVolumeJoint(world, constantVolumeJointDef));
-    }
-  }
-  @override
-  Body createBody() {
-    final shape = CircleShape()..radius = circleShapeRadius;
-    final fixtureDef = FixtureDef(
-      shape,
-      restitution: 1,
-      density: 20.0,
-      friction: 0.0,
-    );
-    final bodyDef = BodyDef(
-      userData: this,
-      angularDamping: 0.0,
-      position: entityPosition,
-      type: BodyType.dynamic,
-      linearVelocity: linearVelocity,
-    );
-    final body = world.createBody(bodyDef);
-    body.createFixture(fixtureDef);
-    return body;
-  }
-  @override
-  void renderCircle(Canvas canvas, Offset center, double radius) {
-    super.renderCircle(canvas, center, radius);
-    final lineRotation = Offset(0, radius);
-    canvas.drawLine(center, center + lineRotation, Paint()..color = Colors.white);
   }
 }
